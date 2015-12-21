3cef3167d3e73655ca90613bfeca9b96403a6422
Phan Thanh Tín <phantin612@gmail.com>
2015-12-21 18:05:09 +0700

    exercise 1.2

--- app/src/main/java/com/tin612/exercise1/MainActivity.java
+++ app/src/main/java/com/tin612/exercise1/MainActivity.java
@@ -3,6 +3,10 @@ package com.tin612.exercise1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,21 +14,104 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     Button nextBtn;
     EditText email;
-    void init(){
-        nextBtn = (Button)findViewById(R.id.nextButton);
-        email = (EditText)findViewById(R.id.emailEditText);
+    EditText fname;
+    EditText lname;
+    EditText pnum;
+    void init() {
+        nextBtn = (Button) findViewById(R.id.nextButton);
+        email = (EditText) findViewById(R.id.emailEditText);
+        pnum = (EditText) findViewById(R.id.p_numEditText);
+        fname = (EditText) findViewById(R.id.fnameEditText);
+        lname = (EditText) findViewById(R.id.lnameEditText);
+    }
+    public final static boolean isValidEmail(CharSequence target) {
+            return Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
+    public final static boolean isValidPhone(CharSequence target) {
+        return Patterns.PHONE.matcher(target).matches();
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if( getIntent().getBooleanExtra("Exit", false)){
+            finish();
+            return; // add this to prevent from doing unnecessary stuffs
+        }
         init();
+//        if(fname.getText().toString().length() == 0) fname.setError("You must enter First Name");
+//        if(lname.getText().toString().length()==0) lname.setError("You must enter Last Name");
+//        if(pnum.getText().toString().length()==0) pnum.setError("You must enter Last Name");
+//        if(email.getText().toString().length()==0) email.setError("You must enter Last Name");
+//        if(!isValidEmail(email.getText().toString())) email.setError("Email is invalid");
+        fname.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus)
+                {
+                    //if(fname.getText().toString().length() == 0) fname.setError("You must enter First Name");
+                }
+                else
+                {
+                    if(fname.getText().toString().length() == 0) fname.setError("You must enter First Name");
+                }
+            }
+        });
+        lname.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus)
+                {
+                    //if(lname.getText().toString().length() == 0) fname.setError("You must enter First Name");
+                }
+                else
+                {
+                    if(lname.getText().toString().length() == 0) lname.setError("You must enter Last Name");
+                }
+            }
+        });
+        email.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus)
+                {
+                    if(!isValidEmail(email.getText().toString())) email.setError("Email is invalid");
+                }
+                else
+                {
+                    if(email.getText().toString().length() == 0)
+                        email.setError("You must enter Last Name");
+                    if(!isValidEmail(email.getText().toString())) email.setError("Email is invalid");
+                }
+            }
+        });
+        pnum.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus)
+                {
+                    if(!isValidPhone(email.getText().toString())) pnum.setError("Email is invalid");
+                }
+                else
+                {
+                    if(pnum.getText().toString().length() == 0)
+                        pnum.setError("You must enter Last Name");
+                    if(!isValidEmail(email.getText().toString())) pnum.setError("Email is invalid");
+                }
+            }
+        });
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent step2 = new Intent(MainActivity.this, Step2.class);
+
                 startActivity(step2);
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
--- app/src/main/java/com/tin612/exercise1/Step2.java
+++ app/src/main/java/com/tin612/exercise1/Step2.java
@@ -1,32 +1,69 @@
 package com.tin612.exercise1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Process;
 
 public class Step2 extends AppCompatActivity {
     Button doneBtn ;
     SeekBar seekBar;
     TextView salary;
+    CheckBox check1;
+    CheckBox check2;
+    CheckBox check3;
+    CheckBox check4;
+    CheckBox check5;
+    CheckBox check6;
     void init(){
         doneBtn = (Button)findViewById(R.id.doneButton);
         seekBar = (SeekBar)findViewById(R.id.salarySeekBar);
         salary = (TextView)findViewById(R.id.salaryTextView);
+        check1 = (CheckBox)findViewById(R.id.checkbox1);
+        check2 = (CheckBox)findViewById(R.id.checkbox2);
+        check3 = (CheckBox)findViewById(R.id.checkbox3);
+        check4 = (CheckBox)findViewById(R.id.checkbox4);
+        check5 = (CheckBox)findViewById(R.id.checkbox5);
+        check6 = (CheckBox)findViewById(R.id.checkbox6);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step2);
         init();
-        seekBar.incrementProgressBy(100);
+        doneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(check1.isChecked() || check2.isChecked() || check3.isChecked() || check4.isChecked()
+                        || check5.isChecked() || check6.isChecked()
+                        )
+                {
+                    Intent intent = new Intent(Step2.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    intent.putExtra("Exit", true);
+                    startActivity(intent);
+                    finish();
+                } else
+                {
+                    Toast.makeText(getApplicationContext(),"You must select one kind of sports",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             Integer value = 0;
+
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                value = progress;
+                progress = ((int) Math.round(progress / 100)) * 100;
+                seekBar.setProgress(progress);
+                salary.setText("Your salary: " + progress + " dollars");
             }
 
             @Override
@@ -36,7 +73,7 @@ public class Step2 extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                salary.setText("Your salary: "+value.toString() + " dollars");
+
             }
         });
     }
@@ -44,5 +81,12 @@ public class Step2 extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Process.killProcess(android.os.Process.myPid());
+        super.onDestroy();
+        getParent().finish();
     }
 }
